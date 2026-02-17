59930413e08d9a104aeb011c299f4a78c254bc08
Madhusai Pathakoti <83114463+MadhusaiPathakoti@users.noreply.github.com>
2026-02-17 20:53:10 +0530

    Change database connection string in db_config.py
    Updated database connection string to use a new host.

--- config/db_config.py
+++ config/db_config.py
@@ -3,6 +3,5 @@ from sqlalchemy import create_engine
 def get_engine():
     engine = create_engine(
         # "mysql+pymysql://USERNAME:PASSWORD@HOST:3306/customer_intelligence"
-        "mysql+pymysql://sql12817317:pcdW1whp9N@sql12.freesqldatabase.com:3306/sql12817317"
     )
     return engine
